57aa8cd3fa043ea8bb4944b195f5b1a63a86f201
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-02-20 00:39:34 +0100

    style & header setup

--- web/includes/header.php
+++ web/includes/header.php
@@ -1,7 +1,25 @@
-<html>
+<html lang="da">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bakkestien Vissenbjerg - Hjerteforeningen</title>
+        
+        <!-- font awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        
+        <!-- custom -->
+        <link href="../assets/css/style.css" rel="stylesheet" type="text/css"/>
+        <script src="../assets/js/script.js" async defer></script>
     </head>
     <body>
-
+        <header>
+            <div class="logo">
+                <a href="index.php">
+                    <em class="fas fa-heart"></em>
+                </a>
+            </div>
+            
+            <nav>
+                <em class="fas fa-bars"></em>
+            </nav>
+        </header>

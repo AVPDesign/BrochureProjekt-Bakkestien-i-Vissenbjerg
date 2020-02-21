36cd3bd5e31f05556365b802f4a036ed1d4c497b
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-02-21 16:54:37 +0100

    css update

--- web/pages/home.php
+++ web/pages/home.php
@@ -1,13 +1,27 @@
 <main>
-    <section class="row">
+    <div class="row">
         <div class="col-1">
-            <h1>Bakkestien i Vissenbjerg</h1>
+            <h1>Bakkestien i Vissenbjerg - Assens</h1>
         </div>
-    </section>
+    </div>
 
-    <section class="row">
+    <div class="row">
         <div class="col-1">
             <div id="map"></div>
         </div>
-    </section>
+    </div>
+    
+    <div class="row">
+        <div class="col-1"></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-2"></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+    </div>
 </main>

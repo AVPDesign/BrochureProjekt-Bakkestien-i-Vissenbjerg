7ce32d649029cb5fbb98ecfd7b7f4c7c77fdd056
Rasmus Andersen <rasmus.skovhaven@gmail.com>
2020-02-26 10:05:52 +0100

    Fnis

---
+++ web/pages/home.php
@@ -0,0 +1,27 @@
+<main>
+    <div class="row">
+        <div class="col-1">
+            <h1>Bakkestien i Vissenbjerg - Assens</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1">
+            <div id="map"></div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-1"></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-2"></div>
+    </div>
+    
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+    </div>
+</main>

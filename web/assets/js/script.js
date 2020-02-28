d547a17a484f1abdaa78531b1fbcc2a1ff67d0fc
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-02-28 22:06:09 +0100

    mobile search implemented

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -15,8 +15,32 @@ function debounce(func, wait, immediate) {
         if (callNow)
             func.apply(context, args);
     };
-}
-;
+};
+
+// Toggle mobile search input
+var toggleMobileSearch = false;
+
+var toggleSearch = debounce(function () {
+    var mobileSearch = document.getElementById('mobileSearch');
+    var mobileSearchPath = document.getElementById('mobileSearchPath');
+    
+    if(toggleMobileSearch == false){
+        mobileSearch.style.opacity = "1";
+        mobileSearch.style.width = "162px";
+        
+        mobileSearchPath.setAttribute("d", "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z");
+
+        toggleMobileSearch = true;
+    }else{
+        mobileSearch.style.opacity = "0";
+        mobileSearch.style.width = "0";
+        
+        mobileSearchPath.setAttribute("d", "M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z");
+
+        
+        toggleMobileSearch = false;
+    }
+}, 250);
 
 // Toggle mobile nav
 var toggle = false;
--- web/includes/header.php
+++ web/includes/header.php
@@ -23,10 +23,10 @@
             <div class="wrapper">
                 <div class="logo">
                     <a href="index.php">
-                        <svg viewBox="0 0 24 24">
+<!--                        <svg viewBox="0 0 24 24">
                         <path fill="currentColor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M9.75,7.82C8.21,7.82 7,9.03 7,10.57C7,12.46 8.7,14 11.28,16.34L12,17L12.72,16.34C15.3,14 17,12.46 17,10.57C17,9.03 15.79,7.82 14.25,7.82C13.38,7.82 12.55,8.23 12,8.87C11.45,8.23 10.62,7.82 9.75,7.82Z" />
-                        </svg>
-                        <!--<img src="assets/img/logo3.svg" alt="logo"/>-->
+                        </svg>-->
+                        <img src="assets/img/logo1.svg" alt="logo"/>
                     </a>
                 </div>
 
@@ -39,10 +39,21 @@
                         <a href="index.php?page=contact" class="<?php if($_GET['page'] == "contact"){echo 'activeDesktop';}?>">Kontakt</a>
                     </div>
                     
-                    <!-- mobile nav -->
-                    <svg id="mobileNav" style="width:2.2em;" viewBox="0 0 24 24" onclick="toggleMobileNav()">
-                    <path fill="white" id="mobileNavMenuPath" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
-                    </svg>
+                    <div class="flex">
+                        <!-- search -->
+                        <div class="flex">
+                            <svg style="width:2em;" viewBox="0 0 24 24" onclick="toggleSearch()">
+                            <path id="mobileSearchPath" fill="#C8102E" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                            </svg>
+                            
+                            <input id="mobileSearch" type="text" placeholder="Søg">
+                        </div>
+
+                        <!-- mobile nav -->
+                        <svg id="mobileNav" style="width:2.2em;" viewBox="0 0 24 24" onclick="toggleMobileNav()">
+                        <path fill="#C8102E" id="mobileNavMenuPath" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
+                        </svg>
+                    </div>
                 </nav>
             </div><!-- wrapper -->
         </header>
@@ -55,4 +66,8 @@
                 <li><a href="index.php?page=about">Om os</a></li>
                 <li><a href="index.php?page=contact">Kontakt</a></li>
             </ul>
+            
+            <div id="mobileNavFooter">
+                <img src="assets/img/hjerteforeningenLogo1.svg" alt="logo"/>
+            </div>
         </div>

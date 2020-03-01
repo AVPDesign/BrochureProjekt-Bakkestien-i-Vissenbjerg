b08693b62b799c72c569c62c1c76e9eeda617ca3
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-03-01 22:34:03 +0100

    fnis

--- web/pages/contact.php
+++ web/pages/contact.php
@@ -8,7 +8,7 @@
     
     <div class="row">
         <div class="col-1">
-            Har du brug for at rette i dine oplysninger, tilmelde dig PBS eller andet, <a href="https://hjerteforeningen.dk/selvbetjening/" taget="_blank">så kan du med fordel benytte vores selvbetjening.</a>
+            Har du brug for at rette i dine oplysninger, tilmelde dig PBS eller andet, <a href="https://hjerteforeningen.dk/selvbetjening/" target="_blank">så kan du med fordel benytte vores selvbetjening.</a>
         </div>
     </div>
     
@@ -18,7 +18,7 @@
             <ul class="m-l">
                 <li>Ring til os på 70 25 00 00 alle hverdage fra kl. 9-15</li>
                 <li><a href="mailto:post@hjerteforeningen.dk">Skriv til os på post@hjerteforeningen.dk</a></li>
-                <li>Medier og journalister er velkomne i <a href="https://hjerteforeningen.dk/presse/">vores presserum</a></li>
+                <li>Medier og journalister er velkomne i <a href="https://hjerteforeningen.dk/presse/" target="_blank">vores presserum</a></li>
                 <li>Følg os på <a href="https://www.facebook.com/hjerteforeningen/" target="_blank">Facebook</a>, <a href="https://twitter.com/Hjerteforening" target="_blank">Twitter</a> og <a href="https://www.instagram.com/hjerteforeningen/?hl=da" target="_blank">Instagram</a></li>
             </ul>
         </div>

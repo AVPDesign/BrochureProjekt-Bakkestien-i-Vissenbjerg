d2aa083a65dba8d779c1bd37448dc9ba83f9fbf3
Rasmus Andersen <rasmus.skovhaven@gmail.com>
2020-03-05 13:10:30 +0100

    Login page implemented

---
+++ web/pages/login.php
@@ -0,0 +1,24 @@
+<main>
+    <div class="row">
+        <div class="col-2 m-x">
+            <form id="logInForm">
+                <label for="email">
+                    Email
+                    <input type="email" placeholder="Indtast din email"/>
+                </label>
+                <label for="email">
+                    Adgangskode
+                    <input type="email" placeholder="Indtast din adgangskode"/>
+                </label>
+                
+                <label class="spec-label">
+                    <input type="checkbox" value="1"/>
+                    Forbliv logget ind
+                </label>
+                
+                <input type="submit" value="Log ind"/>
+            </form>
+            <a href="#" style="font-size:.8em;line-height: 2.5em;">Glemt adgangskode?</a>
+        </div>
+    </div>
+</main>
